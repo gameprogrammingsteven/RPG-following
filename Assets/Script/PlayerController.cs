928b8b72e493a2cd1c3581c73ab4c528bf5bbf75
Stephen J <sjjair@yahoo.com>
2022-12-14 11:52:35 -0800

    added: Anim controller with blends

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rigidBody;
+    public float playerSpeed = 800;
+
+    public Animator animator;
+
+    bool safeCheck = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(rigidBody != null && animator != null) //performant safety in code;
+        {
+            safeCheck = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!safeCheck)
+        {
+            return;
+        }
+
+        float hVelocity = (Input.GetAxisRaw("Horizontal") * Time.deltaTime * playerSpeed);
+        float yVelocity = (Input.GetAxisRaw("Vertical") * Time.deltaTime * playerSpeed);
+
+        rigidBody.velocity = new Vector2(hVelocity, yVelocity);
     }
 }
